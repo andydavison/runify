8b75d7920043b59f14e9e31ea50a28a61438dfad
Andy Davison <ajdvsn@gmail.com>
2020-05-10 14:49:09 +0100

    Updates

---
+++ precache-manifest.c927bb26a92a89d1f404d4a0043b0b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91ce4a16c0e08b8542a5d928b5337a7",
+    "url": "/runify/index.html"
+  },
+  {
+    "revision": "a43cef512d8c3d322cee",
+    "url": "/runify/static/css/main.409e43ff.chunk.css"
+  },
+  {
+    "revision": "d16a7134e8e10d6bfcec",
+    "url": "/runify/static/js/2.24513566.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/runify/static/js/2.24513566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a43cef512d8c3d322cee",
+    "url": "/runify/static/js/main.91fea3e0.chunk.js"
+  },
+  {
+    "revision": "71f89a10f09f35974504",
+    "url": "/runify/static/js/runtime-main.57906ded.js"
+  }
+]);
